76b0bf19b345a18eb8cefd0430c0ad916cc3fc84
Alex Schneider <me@schneider.ax>
2025-03-26 12:49:57 +0700

    Draft files source

---
+++ src/sources/files.ts
@@ -0,0 +1,46 @@
+import type { Source } from '../types.ts'
+
+interface FilesSourceOptions {
+  name?: string
+}
+
+function files(): Source<FilesSourceOptions> {
+  return (options) => {
+    const { name } = options
+    return [
+      {
+        id: 'files',
+        tags: ['file'],
+        value: [
+          ...(name
+            ? [
+                'package.json',
+                `.${name}rc`,
+                `.${name}rc.json`,
+                `.${name}rc.yaml`,
+                `.${name}rc.yml`,
+                `.${name}rc.js`,
+                `.${name}rc.ts`,
+                `.${name}rc.mjs`,
+                `.${name}rc.cjs`,
+                `.config/${name}rc`,
+                `.config/${name}rc.json`,
+                `.config/${name}rc.yaml`,
+                `.config/${name}rc.yml`,
+                `.config/${name}rc.js`,
+                `.config/${name}rc.ts`,
+                `.config/${name}rc.mjs`,
+                `.config/${name}rc.cjs`,
+                `${name}.config.js`,
+                `${name}.config.ts`,
+                `${name}.config.mjs`,
+                `${name}.config.cjs`,
+              ]
+            : []),
+        ],
+      },
+    ]
+  }
+}
+
+export default files
