9d6473af18c551a2e395e652c3ea6bf8a7ba03a5
Alex Schneider <me@schneider.ax>
2025-03-26 12:44:44 +0700

    Draft cosmiconfig loader

---
+++ src/loaders/cosmiconfig.ts
@@ -0,0 +1,42 @@
+import { cosmiconfig, cosmiconfigSync } from 'cosmiconfig'
+import type { Loader } from '../types.ts'
+
+interface CosmiconfigLoaderOptions {
+  name?: string
+}
+
+const cosmiconfigLoader: Loader<CosmiconfigLoaderOptions> = {
+  name: 'cosmiconfig',
+  canLoadSync: (entry) => {
+    return Boolean(entry.tags?.includes('dir') || entry.tags?.includes('file'))
+  },
+  loadSync: (entry, options) => {
+    const searchPlaces = Array.isArray(entry.value) ? entry.value : [entry.value]
+    const explorer = cosmiconfigSync(options.name || '', { searchPlaces })
+    const result = explorer.search()
+    return [
+      {
+        ...entry,
+        loaded: true,
+        value: result?.config || {},
+      },
+    ]
+  },
+  canLoadAsync: async (entry) => {
+    return Boolean(entry.tags?.includes('dir') || entry.tags?.includes('file'))
+  },
+  loadAsync: async (entry, options) => {
+    const searchPlaces = Array.isArray(entry.value) ? entry.value : [entry.value]
+    const explorer = cosmiconfig(options.name || '', { searchPlaces })
+    const result = await explorer.search()
+    return [
+      {
+        ...entry,
+        loaded: true,
+        value: result?.config || {},
+      },
+    ]
+  },
+}
+
+export default cosmiconfigLoader
