6da41e287bf08fae81a865df68eb86e53de95757
Alex Schneider <me@schneider.ax>
2025-04-08 19:31:33 +0700

    Revamp configFile source to work with fileLoader and jsonMapper

--- src/sources/configFile.ts
+++ src/sources/configFile.ts
@@ -13,9 +13,22 @@ function configFile(options?: ConfigFileSourceOptions): Source<ConfigFileSourceO
     if (!name) return []
     return [
       {
-        id: 'configFile',
-        format: ['configFile'],
-        value: name,
+        id: 'configFiles',
+        format: ['file'],
+        value: [
+          `.${name}rc.json`,
+          `.${name}rc.js`,
+          `.${name}rc.cjs`,
+          `.${name}rc.mjs`,
+          `.config/${name}rc`,
+          `.config/${name}rc.json`,
+          `.config/${name}rc.js`,
+          `.config/${name}rc.cjs`,
+          `.config/${name}rc.mjs`,
+          `${name}.config.js`,
+          `${name}.config.cjs`,
+          `${name}.config.mjs`,
+        ],
       },
     ]
   }
