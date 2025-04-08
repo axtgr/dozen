3ff3cb826854769930bf86a99e0ac9f636168b01
Alex Schneider <me@schneider.ax>
2025-04-08 19:05:12 +0700

    Make a distinction between add and change events in fileWatcher

--- src/fileWatcher.ts
+++ src/fileWatcher.ts
@@ -4,18 +4,17 @@ import { FSWatcher } from 'chokidar'
 import type { Entry, PluginWatchCb } from './types.ts'
 
 function createFileWatcher() {
-  const watchedEntries = new Map<string, Entry>()
+  const watchedEntries = new Map<string, [Entry, Entry?]>()
   const watchCbs = new Set<PluginWatchCb>()
   let chokidar: FSWatcher | undefined
 
   const onChange = (eventName: string, filePath: string) => {
     if (eventName === 'error') return
     if (watchedEntries.has(filePath)) {
-      const entry = watchedEntries.get(filePath)!
+      const [parentEntry, childEntry] = watchedEntries.get(filePath)!
+      const entry = eventName === 'add' ? parentEntry : childEntry || parentEntry
       entry.status = 'pending'
       watchCbs.forEach((cb) => cb(undefined, entry))
-    } else {
-      chokidar?.unwatch(filePath)
     }
   }
 
@@ -24,11 +23,18 @@ function createFileWatcher() {
   }
 
   return {
-    add(filePath: string, entry: Entry) {
+    /**
+     * Adds a file path to the watcher.
+     *
+     * If a file is created at the path, the parent entry will be emitted.
+     * If a file is modified at the path, the child entry will be emitted.
+     */
+    add(filePath: string, parentEntry: Entry, childEntry?: Entry) {
       // Watch for the dirname instead of the full file path to catch file creation
       chokidar?.add(Path.dirname(filePath))
-      watchedEntries.set(filePath, entry)
+      watchedEntries.set(filePath, [parentEntry, childEntry])
     },
+
     async watch(cb: PluginWatchCb) {
       watchCbs.add(cb)
       if (watchCbs.size === 1) {
@@ -40,7 +46,9 @@ function createFileWatcher() {
         watchedEntries.keys().forEach((filePath) => chokidar!.add(filePath))
       }
     },
+
     async unwatch(cb: (err?: unknown, entry?: Entry) => void) {
+      console.log('unwatch')
       watchCbs.delete(cb)
       if (!watchCbs.size) {
         await chokidar?.close()
