f0deb81662cdfc52c02dbd7379eb79a9f4d179e7
Alex Schneider <me@schneider.ax>
2025-04-18 00:01:16 +0700

    Remove unused types and type-fest lib

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,4 @@
 import * as textCase from 'text-case'
-import type { KeysOfUnion, Simplify } from 'type-fest'
-
-type UnionOptionalInner<
-  BaseType extends object,
-  EveryKey extends KeysOfUnion<BaseType> = KeysOfUnion<BaseType>,
-> = Simplify<
-  // 1. For each member of the union (Note: `T extends any` is distributive)
-  BaseType extends object
-    ? // 2. Preserve the original type
-      BaseType & { [K in Exclude<EveryKey, keyof BaseType>]?: unknown } // 3. And map other keys to `{ key?: unknown }`
-    : never
->
-
-/**
- * Preserves the union, but makes "Property does not exist on type" errors go away
- * by marking them as `{ key?: unknown }`
- *
- * Based on https://x.com/buildsghost/status/1843513210001138076, but uses `unknown` instead of `undefined`
- */
-type UnionOptional<BaseType extends object> = UnionOptionalInner<BaseType>
 
 function isObjectOrFunction<T extends object>(value: T | null | undefined | false): value is T {
   return typeof value === 'function' || (typeof value === 'object' && value !== null)
@@ -53,4 +33,3 @@ const stringCases = {
 }
 
 export { isObjectOrFunction as isObject, toFilteredArray, stringCases }
-export type { UnionOptional }
